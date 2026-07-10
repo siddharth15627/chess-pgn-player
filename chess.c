cc670938ea0691eb3f6399be76498330e5715fd4
Siddharth Aarthi <siddharth15627@gmail.com>
2026-07-10 20:53:49 +0530

    Code for castling implemented

--- chess.c
+++ chess.c
@@ -53,10 +53,10 @@ Game * read_game() {
     // TODO Read from file instead?
     char *moves[] = {
         // White wins, no castling
-        "e2e4", "c7c5", "g1f3", "g8f6", "b1c3", "e7e6", "d2d4", "c5d4", "f3d4", "f8b4", "e4e5", "f6d5", "c1d2", "d5c3", "b2c3", "b4f8", "f1d3", "d7d6", "d1e2", "b8d7", "d4e6", "d8b6", "e6c7", "1-0"
+        // "e2e4", "c7c5", "g1f3", "g8f6", "b1c3", "e7e6", "d2d4", "c5d4", "f3d4", "f8b4", "e4e5", "f6d5", "c1d2", "d5c3", "b2c3", "b4f8", "f1d3", "d7d6", "d1e2", "b8d7", "d4e6", "d8b6", "e6c7", "1-0"
         
         // Draw, with castling at e1c1 (white) and e8g8 (black)
-        //"e2e4", "e7e5", "g1f3", "g8f6", "f3e5", "d7d6", "e5f3", "f6e4", "d1e2", "d8e7", "d2d3", "e4f6", "c1g5", "b8d7", "b1c3", "e7e2", "f1e2", "h7h6", "g5h4", "g7g6", "d3d4", "a7a6", "e1c1", "f8g7", "h1e1", "e8g8", "e2c4", "b7b5", "c4d5", "a8b8", "d5c6", "d7b6", "a2a3", "c8b7", "c6b7", "b8b7", "h4f6", "1/2-1/2"
+        "e2e4", "e7e5", "g1f3", "g8f6", "f3e5", "d7d6", "e5f3", "f6e4", "d1e2", "d8e7", "d2d3", "e4f6", "c1g5", "b8d7", "b1c3", "e7e2", "f1e2", "h7h6", "g5h4", "g7g6", "d3d4", "a7a6", "e1c1", "f8g7", "h1e1", "e8g8", "e2c4", "b7b5", "c4d5", "a8b8", "d5c6", "d7b6", "a2a3", "c8b7", "c6b7", "b8b7", "h4f6", "1/2-1/2"
 
         // Black wins, with promotion at g2f1Q (black)
         //"e2e4", "e7e5", "g1f3", "b8c6", "f1c4", "f8e7", "e1g1", "g8f6", "b1c3", "e8g8", "d2d3", "d7d6", "c1e3", "c8g4", "d1e2", "c6a5", "c4b3", "a5b3", "a2b3", "a7a6", "b3b4", "d6d5", "f1d1", "d5d4", "e3d2", "d4c3", "d2c3", "f6d7", "d1f1", "g8h8", "e2e3", "e7d6", "a1d1", "f7f5", "d3d4", "f5e4", "d4e5", "e4f3", "e5d6", "f3g2", "e3d4", "g2f1Q", "d1f1", "d8g5", "d4g7", "g5g7", "c3g7", "h8g7", "0-1"
@@ -128,9 +128,32 @@ void update_board(Board *b, char *move, bool is_white) {
     // If this is a capture, the piece at dst is overwritten
     strncpy(b->cell[dst.x][dst.y], b->cell[src.x][src.y], 4);
     strncpy(b->cell[src.x][src.y], " ", 4);
-
-    // TODO Castling
     
+    // Castling : Rook move given below
+    if (is_white && src.x == 7 && src.y == 4) {
+        //long castling - white
+        if (dst.x == 7 && dst.y == 2) {
+            strncpy(b->cell[7][3], b->cell[7][0], 4);
+            strncpy(b->cell[7][0], " ", 4);
+        }
+        // short castle - white
+        else if(dst.x == 7 && dst.y == 6) {
+            strncpy(b->cell[7][5], b->cell[7][7], 4);
+            strncpy(b->cell[7][7], " ", 4);
+        }
+    }
+    else if (!is_white && src.x == 0 && src.y == 4) {
+        //long castling -  black
+        if(dst.x == 0 && dst.y == 2) {
+            strncpy(b->cell[0][3], b->cell[0][0], 4);
+            strncpy(b->cell[0][0], " ", 4);
+        }
+        //short casting - black
+        else if(dst.x == 0 && dst.y == 6) {
+            strncpy(b->cell[0][5], b->cell[0][7], 4);
+            strncpy(b->cell[0][7], " ", 4);
+        }
+    }
     // TODO Promotion
 }
 
